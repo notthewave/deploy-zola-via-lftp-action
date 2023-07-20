56f338bc7ffb1ad7c192b15f9741295d3db5a9e4
notthewave <74874782+notthewave@users.noreply.github.com>
2023-07-21 00:28:58 +0200

    git Las

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
+git lfs fetch --all
 zola build || exit 1
 
 lftp -f "
 open -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -p $INPUT_FTP_PORT -e ls $INPUT_FTP_HOST || exit 1
-git lfs pull
 mirror --reverse --delete --depth-first --overwrite --verbose $INPUT_ZOLA_BUILD_DIR $INPUT_REMOTE_TARGET_DIR || exit 1
 bye
 " || exit 1
