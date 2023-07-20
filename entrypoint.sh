6f426b8bee323b80ce8c32ac1edb6f8318f9d810
notthewave <74874782+notthewave@users.noreply.github.com>
2023-07-21 00:22:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ zola build || exit 1
 
 lftp -f "
 open -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -p $INPUT_FTP_PORT -e ls $INPUT_FTP_HOST || exit 1
+git lfs pull
 mirror --reverse --delete --depth-first --overwrite --verbose $INPUT_ZOLA_BUILD_DIR $INPUT_REMOTE_TARGET_DIR || exit 1
 bye
 " || exit 1
